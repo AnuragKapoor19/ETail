0a46ddc6574f29ded3677b6b2b883f1fe6d1e8ff
Anurag Kapoor <anuragkapoor.ak47@gmail.com>
2025-01-01 12:16:52 +0530

    developing dashboard

--- frontend/src/components/DashboardSidebar.js
+++ frontend/src/components/DashboardSidebar.js
@@ -1,7 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { MdDashboard } from "react-icons/md";
+import { FaBorderAll, FaPlus, FaProductHunt, FaShoppingBasket, FaUsers } from "react-icons/fa";
+import { TiArrowSortedDown } from "react-icons/ti";
+
 
 export default function DashboardSidebar() {
+  const [dropdown, setdropdown] = useState(false)
+
+  const handleClick = () => {
+    setdropdown(!dropdown)
+  }
+
   return (
-    <div>DashboardSidebar</div>
+    <>
+      <div className='col-sm-3 col-md-2 col-lg-2 me-3 bg-dark text-light sidebar text-center' style={{ height: '100vh' }}>
+        <ul className='mt-5 d-flex align-items-start flex-column' style={{ listStyle: 'none', cursor: 'pointer' }}>
+
+          <li className='my-3' onClick={() => setdropdown(false)}>
+            <Link to='/admin/dashboard' className='text-decoration-none text-light fw-bold fs-5'><MdDashboard /> Dashboard</Link>
+          </li>
+
+          <li className='my-3'>
+            <span onClick={handleClick} className='text-light fw-bold fs-5'><FaProductHunt /> Products <TiArrowSortedDown /></span>
+            {dropdown &&
+              <div className='d-flex flex-column ms-3 mt-2 align-items-start'>
+                <Link to='/admin/dashboard' className='text-decoration-none text-light fw-bold fs-5'><FaBorderAll /> All</Link>
+                <Link to='/admin/dashboard' className='text-decoration-none text-light fw-bold fs-5'><FaPlus /> Create</Link>
+              </div>
+            }
+          </li>
+
+          <li className='my-3' onClick={() => setdropdown(false)}>
+            <Link to='/admin/dashboard' className='text-decoration-none text-light fw-bold fs-5'><FaShoppingBasket /> Orders</Link>
+          </li>
+
+          <li className='my-3' onClick={() => setdropdown(false)}>
+            <Link to='/admin/dashboard' className='text-decoration-none text-light fw-bold fs-5'><FaUsers /> Users</Link>
+          </li>
+
+        </ul>
+      </div>
+    </>
   )
 }
--- frontend/src/pages/Dasboard.js
+++ frontend/src/pages/Dasboard.js
@@ -8,57 +8,59 @@ export default function Dasboard() {
     return (
         <>
             <Header />
-            <DashboardSidebar />
-            <div className="dashboard-container row text-light m-3">
-                <h3 className='col-12 text-dark'>Dashboard</h3>
+            <div className='d-flex col-12'>
+                <DashboardSidebar />
+                <div className="dashboard-container row text-light m-3 col-sm-8 col-md-9 col-lg-9" style={{height: 'fit-content'}}>
+                    <h3 className='col-12 text-dark'>Dashboard</h3>
 
-                <div className='d-flex justify-content-between flex-column align-items-center col-12 p-0'>
+                    <div className='d-flex justify-content-between flex-column align-items-center col-12 p-0'>
 
-                    <div className="total-amount col-12 bg-primary p-3 d-flex flex-column justify-content-center align-items-center">
-                        <span>Total Amount</span>
-                        <b>$4523.63</b>
-                    </div>
-
-                    <div className='d-flex justify-content-between align-items-center col-12 p-0'>
-
-                        <div className="products bg-danger col-3 my-2 p-1 d-flex flex-column justify-content-center align-items-center">
-                            <span>Products</span>
-                            <b>45</b>
-                            <hr className='border-top border-light w-100 m-0 p-0 mt-4'/>
-                            <Link className='btn d-flex justify-content-between w-100'>
-                                <span>View Details</span>
-                                <i><FaChevronRight size='10px' /></i>
-                            </Link>
+                        <div className="total-amount col-12 bg-primary p-3 py-5 d-flex flex-column justify-content-center align-items-center fs-4">
+                            <span>Total Amount</span>
+                            <b>$4523.63</b>
                         </div>
 
-                        <div className="orders bg-info col-3 my-2 p-1 d-flex flex-column justify-content-center align-items-center">
-                            <span>Orders</span>
-                            <b>853</b>
-                            <hr className='border-top border-light w-100 m-0 p-0 mt-4'/>
-                            <Link className='btn d-flex justify-content-between w-100'>
-                                <span>View Details</span>
-                                <i><FaChevronRight size='10px' /></i>
-                            </Link>
-                        </div>
+                        <div className='d-flex justify-content-between align-items-center col-12 p-0 fs-4'>
 
-                        <div className="users bg-secondary col-3 my-2 p-1 d-flex flex-column justify-content-center align-items-center">
-                            <span>Users</span>
-                            <b>45</b>
-                            <hr className='border-top border-light w-100 m-0 p-0 mt-4'/>
-                            <Link className='btn d-flex justify-content-between w-100'>
-                                <span>View Details</span>
-                                <i><FaChevronRight size='10px' /></i>
-                            </Link>
-                        </div>
+                            <div className="products bg-danger col-3 my-2 p-1 py-2 d-flex flex-column justify-content-center align-items-center">
+                                <span>Products</span>
+                                <b>45</b>
+                                <hr className='border-top border-light w-100 m-0 p-0 mt-4' />
+                                <Link className='btn d-flex justify-content-between w-100'>
+                                    <span>View Details</span>
+                                    <i><FaChevronRight size='10px' /></i>
+                                </Link>
+                            </div>
 
-                        <div className="out-of-stock bg-success col-3 my-2 p-1 d-flex flex-column justify-content-center align-items-center">
-                            <span>Out of Stock</span>
-                            <b>4</b>
-                            <hr className='border-top border-light w-100 m-0 p-0 mt-4'/>
-                            <Link className='btn d-flex justify-content-between w-100'>
-                                <span>View Details</span>
-                                <i><FaChevronRight size='10px' /></i>
-                            </Link>
+                            <div className="orders bg-info col-3 my-2 p-1 py-2 d-flex flex-column justify-content-center align-items-center">
+                                <span>Orders</span>
+                                <b>853</b>
+                                <hr className='border-top border-light w-100 m-0 p-0 mt-4' />
+                                <Link className='btn d-flex justify-content-between w-100'>
+                                    <span>View Details</span>
+                                    <i><FaChevronRight size='10px' /></i>
+                                </Link>
+                            </div>
+
+                            <div className="users bg-secondary col-3 my-2 p-1 py-2 d-flex flex-column justify-content-center align-items-center">
+                                <span>Users</span>
+                                <b>45</b>
+                                <hr className='border-top border-light w-100 m-0 p-0 mt-4' />
+                                <Link className='btn d-flex justify-content-between w-100'>
+                                    <span>View Details</span>
+                                    <i><FaChevronRight size='10px' /></i>
+                                </Link>
+                            </div>
+
+                            <div className="out-of-stock bg-success col-3 my-2 p-1 py-2 d-flex flex-column justify-content-center align-items-center">
+                                <span>Unavailable</span>
+                                <b>4</b>
+                                <hr className='border-top border-light w-100 m-0 p-0 mt-4' />
+                                <Link className='btn d-flex justify-content-between w-100'>
+                                    <span>View Details</span>
+                                    <i><FaChevronRight size='10px' /></i>
+                                </Link>
+                            </div>
                         </div>
                     </div>
                 </div>
