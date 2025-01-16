c09d9958a85c7ba7f06c7eb897a480e17abaad01
Anurag Kapoor <anuragkapoor.ak47@gmail.com>
2025-01-16 19:23:05 +0530

    initial commit

--- frontend/src/components/HomePageComponents/Fashion.js
+++ frontend/src/components/HomePageComponents/Fashion.js
@@ -8,18 +8,18 @@ export default function Fashion() {
                 <h3 className='fw-bold mb-4'>The days get cooler, so do the trends</h3>
                 <div className='big d-flex'>
                     <div className='men-fashion'>
-                        <img className='fashion-image' src='https://static.vecteezy.com/system/resources/thumbnails/028/714/265/small_2x/fashion-week-male-model-isolated-on-gradient-background-with-a-place-for-text-photo.jpg' alt='men-fashion' />
-                        <div className='fashion-content text-light w-50'>
-                            <h1 className='fw-bolder'>Because Confidence is Always in Style.</h1>
-                            <div className='btn btn-light rounded-5'>Shop now</div>
+                        <img className='fashion-image' src='https://res.cloudinary.com/anuragkapoor/image/upload/t_Fashion2/v1737025956/Fashion.png' alt='men-fashion' />
+                        <div className='fashion-content text-light'>
+                            <h1 className='menh1'>Fashion for the man who leads, not follows.</h1>
+                            <div className='fashion-btn btn btn-light rounded-5'>Shop now</div>
                         </div>
                     </div>
 
                     <div className='women-fashion'>
                         <img className='fashion-image' src='https://d2line.com/thatlook/wp-content/uploads/sites/4/2022/02/40s-women-fashion-d2line-tips.png' alt='women-fashion' />
-                        <div className='fashion-content w-50'>
-                            <h1 className='fw-bolder'>Fashion That Celebrates the Queen in You.</h1>
-                            <div className='btn btn-dark rounded-5'>Shop now</div>
+                        <div className='fashion-content'>
+                            <h1>Fashion That Celebrates the Queen in You.</h1>
+                            <div className='fashion-btn btn btn-dark rounded-5'>Shop now</div>
                         </div>
                     </div>
                 </div>
--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -43,22 +43,32 @@ export default function Login() {
 
     return (
         <>
-            <div className="login-form d-flex justify-content-center align-items-center bg-danger" style={{ height: '100vh' }}>
-                <div className="login border border-warning rounded-5 p-3 col-sm-8 col-md-4 col-lg-3 bg-light" >
-                    <h3 className='text-center fs-1'>Login</h3>
-                    <form className='d-flex flex-column' onSubmit={handleSubmit}>
-                        <div className="mt-3 mb-3">
-                            <label htmlFor="email" className="form-label">Email address</label>
-                            <input type="email" name='email' className="form-control border-warning fw-bold" id="email" placeholder="name@example.com" value={credentials.email} onChange={handleChange} required />
-                        </div>
-                        <div>
-                            <label htmlFor="password" className="form-label">Password</label>
-                            <input type="password" name='password' className="form-control border-warning fw-bold" id="password" placeholder="password" value={credentials.password} onChange={handleChange} required />
-                        </div>
-                        <Link to='/forgotpassword' className="text-decoration-none">Forgot Password?</Link>
-                        <button className='btn btn-warning mt-5' type='submit'>Login</button>
-                        <Link to='/signin' className='text-decoration-none text-center'>Not a Member? Signup</Link>
-                    </form>
+            <div className='login-page'>
+                <div className='div1' />
+                <div className='div2' />
+                <div className='login-container d-flex'>
+                    <div className='login-form col-6'>
+                        <h3 className='text-center fs-1'>Login</h3>
+                        <form className='d-flex flex-column' onSubmit={handleSubmit}>
+                            <div className="mt-3 mb-3">
+                                <label htmlFor="email" className="form-label">Email address</label>
+                                <input type="email" name='email' className="form-control border-warning fw-bold" id="email" placeholder="name@example.com" value={credentials.email} onChange={handleChange} required />
+                            </div>
+                            <div>
+                                <label htmlFor="password" className="form-label">Password</label>
+                                <input type="password" name='password' className="form-control border-warning fw-bold" id="password" placeholder="password" value={credentials.password} onChange={handleChange} required />
+                            </div>
+                            <Link to='/forgotpassword' className="text-decoration-none">Forgot Password?</Link>
+                            <button className='btn btn-warning mt-5' type='submit'>Login</button>
+                            <Link to='/signin' className='text-decoration-none text-center'>Not a Member? Signup</Link>
+                        </form>
+                    </div>
+
+                    <div className='login-image-container col-4'>
+                        <img src='https://static.vecteezy.com/system/resources/thumbnails/012/697/735/small_2x/red-and-white-abstract-background-with-geometric-elements-for-modern-presentation-template-design-free-vector.jpg' alt='welcome' />
+                        <h1>Welcome to ETail!</h1>
+                        <span>Your shopping journey starts here. Let’s make it amazing!</span>
+                    </div>
                 </div>
             </div>
         </>
